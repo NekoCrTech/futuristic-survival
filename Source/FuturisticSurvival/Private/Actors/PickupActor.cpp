8efb40ceee882fec53447c48acedf7940a23811d
nektpol <nekocreativetechnologies@gmail.com>
2025-10-04 02:03:47 +0300

    Create pickup actor

---
+++ Source/FuturisticSurvival/Private/Actors/PickupActor.cpp
@@ -0,0 +1,34 @@
+// developed by Neko
+
+
+#include "Actors/PickupActor.h"
+
+APickupActor::APickupActor()
+{
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+}
+
+FText APickupActor::GetInteractionText_Implementation()
+{
+	return InteractionText;
+}
+
+void APickupActor::Interact_Implementation(class ASurvCharacter* Caller)
+{
+	Execute_Interact(this,Caller);
+	// class UInventoryComponent* InvComp = Caller->GetInventory();
+	// int rem = -1;
+	// if( rem = InvComp->AddItem(InventoryItem, ItemCount) == 0)
+	// {
+	// 	Interact(Caller)
+	// 	return;
+	// }
+	// ItemCount = rem;
+	// return;
+}
+
+bool APickupActor::IsInteractable_Implementation()
+{
+	return true;
+}
