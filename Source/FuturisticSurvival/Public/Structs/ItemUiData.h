54ad4cfbba08f64fda118e236d06dc6a3cc73c1b
nektpol <nekocreativetechnologies@gmail.com>
2025-10-04 20:35:27 +0300

    Separate data from classes

---
+++ Source/FuturisticSurvival/Public/Structs/ItemUiData.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Logger.h"
+#include "SurvUtils.h"
+#include "Enums/ItemQuality.h"
+#include "ItemUiData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FItemUIData
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FText	ItemName;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FText	ItemDescription;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UTexture2D* ItemIcon;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	EItemQuality ItemQuality;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FLinearColor ItemQualityColor;
+
+	FItemUIData() {};
+
+	FItemUIData(const FText& Name, const FText& Description,UTexture2D* Icon, const EItemQuality& Quality)
+	{
+		ItemName = Name;
+		ItemDescription = Description;
+		ItemIcon = Icon;
+		ItemQuality = Quality;
+		ItemQualityColor = GetItemQualityColor(Quality);
+	}
+};
