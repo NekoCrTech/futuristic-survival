ae5efbaf17bab3bb9408f26f82a41cc058aef41b
nektpol <nekocreativetechnologies@gmail.com>
2025-10-08 21:34:27 +0300

    Update TimeData.h

--- Source/FuturisticSurvival/Public/Structs/TimeData.h
+++ Source/FuturisticSurvival/Public/Structs/TimeData.h
@@ -11,6 +11,8 @@ struct FTimeData
 	GENERATED_BODY()
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, SaveGame)
+	int DayOfYear = 0;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, SaveGame)
 	int Year = 3000;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, SaveGame)
 	int Month = 1;
