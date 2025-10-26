8106c92024c3c597b0d4b261d6abd6e8613f10fd
nektpol <nekocreativetechnologies@gmail.com>
2025-10-26 19:05:15 +0200

    Overlap Test

---
+++ Source/FuturisticSurvival/Public/AbilitySystem/Abilities/Player/SurvPrimary.h
@@ -0,0 +1,31 @@
+﻿// developed by Neko
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilitysystem/Abilities/SurvGameplayAbility.h"
+#include "SurvPrimary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FUTURISTICSURVIVAL_API USurvPrimary : public USurvGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Survival|Ability")
+	void HitBoxOverlapTest();
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Survival|Ability")
+	float HitBoxRadius = 80.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Survival|Ability")
+	float HitBoxForwardOffset = 200.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Survival|Ability")
+	float HitBoxElevationOffset = 20.f;
+};
