c1f8a61171e999a396a7b07d3584ad0edfd0c61d
nektpol <nekocreativetechnologies@gmail.com>
2025-10-26 11:15:12 +0200

    Add Native Gameplay Tags

---
+++ Source/FuturisticSurvival/Private/GameplayTags/SurvTags.cpp
@@ -0,0 +1,10 @@
+﻿#include "GameplayTags/SurvTags.h"
+
+namespace SurvTags
+{
+	namespace SurvAbilities
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Primary, "SurvTags.SurvAbilities.Primary", "Tag for the primary ability")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Secondary, "SurvTags.SurvAbilities.Secondary", "Tag for the secondary ability")
+	}
+}
---
+++ Source/FuturisticSurvival/Public/GameplayTags/SurvTags.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+namespace SurvTags
+{
+	namespace SurvAbilities
+	{
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(Primary);
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(Secondary);
+	}
+}
