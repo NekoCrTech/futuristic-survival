d2b21056c8e264e13b32e9940d33af55ee86905e
nektpol <nekocreativetechnologies@gmail.com>
2025-10-26 11:51:36 +0200

    Base Ability Class

---
+++ Source/FuturisticSurvival/Private/Abilitysystem/Abilities/SurvGameplayAbility.cpp
@@ -0,0 +1,24 @@
+﻿// developed by Neko
+
+
+#include "Abilitysystem/Abilities/SurvGameplayAbility.h"
+
+void USurvGameplayAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	if (bDrawDebug == true)
+	{
+		const FString ActorName = ActorInfo->OwnerActor->GetName();
+		const FString AbilityName = GetName();
+		const FString DebugMessage = FString::Printf(TEXT("The %s activated %s"), *ActorName, *AbilityName);
+
+		GEngine->AddOnScreenDebugMessage(
+			-1,               // key (-1 means create new message)
+			3.f,              // display time
+			FColor::Yellow,   // color
+			DebugMessage      // message
+		);
+	}
+}
---
+++ Source/FuturisticSurvival/Public/Abilitysystem/Abilities/SurvGameplayAbility.h
@@ -0,0 +1,24 @@
+﻿// developed by Neko
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "SurvGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FUTURISTICSURVIVAL_API USurvGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Survival|Debug")
+	bool bDrawDebug = false;
+
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
