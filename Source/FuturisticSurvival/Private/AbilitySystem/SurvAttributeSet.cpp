e65a4fbe8b155ad7479f79188e388a4995d623de
nektpol <nekocreativetechnologies@gmail.com>
2025-10-26 20:51:08 +0200

    Create Attribute Set

---
+++ Source/FuturisticSurvival/Private/AbilitySystem/SurvAttributeSet.cpp
@@ -0,0 +1,72 @@
+﻿// developed by Neko
+
+
+#include "AbilitySystem/SurvAttributeSet.h"
+#include "Net/UnrealNetwork.h"
+
+void USurvAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(USurvAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(USurvAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(USurvAttributeSet, Stamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(USurvAttributeSet, MaxStamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(USurvAttributeSet, Energy, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(USurvAttributeSet, MaxEnergy, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(USurvAttributeSet, Thirst, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(USurvAttributeSet, MaxThirst, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(USurvAttributeSet, Hunger, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(USurvAttributeSet, MaxHunger, COND_None, REPNOTIFY_Always);
+}
+
+/** ---------------- Replication Notifies ---------------- */
+void USurvAttributeSet::OnRep_Health(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USurvAttributeSet, Health, OldValue);
+}
+
+void USurvAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USurvAttributeSet, MaxHealth, OldValue);
+}
+
+void USurvAttributeSet::OnRep_Stamina(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USurvAttributeSet, Stamina, OldValue);
+}
+
+void USurvAttributeSet::OnRep_MaxStamina(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USurvAttributeSet, MaxStamina, OldValue);
+}
+
+void USurvAttributeSet::OnRep_Energy(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USurvAttributeSet, Energy, OldValue);
+}
+
+void USurvAttributeSet::OnRep_MaxEnergy(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USurvAttributeSet, MaxEnergy, OldValue);
+}
+
+void USurvAttributeSet::OnRep_Thirst(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USurvAttributeSet, Thirst, OldValue);
+}
+
+void USurvAttributeSet::OnRep_MaxThirst(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USurvAttributeSet, MaxThirst, OldValue);
+}
+
+void USurvAttributeSet::OnRep_Hunger(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USurvAttributeSet, Hunger, OldValue);
+}
+
+void USurvAttributeSet::OnRep_MaxHunger(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USurvAttributeSet, MaxHunger, OldValue);
+}
---
+++ Source/FuturisticSurvival/Public/AbilitySystem/SurvAttributeSet.h
@@ -0,0 +1,90 @@
+﻿// developed by Neko
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+
+#include "SurvAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+
+UCLASS()
+class FUTURISTICSURVIVAL_API USurvAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+public:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	/** ---------------- Health ---------------- */
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category="Attributes|Vital")
+	FGameplayAttributeData Health;
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldValue);
+	ATTRIBUTE_ACCESSORS(USurvAttributeSet, Health)
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category="Attributes|Vital")
+	FGameplayAttributeData MaxHealth;
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldValue);
+	ATTRIBUTE_ACCESSORS(USurvAttributeSet, MaxHealth)
+
+	/** ---------------- Stamina ---------------- */
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Stamina, Category="Attributes|Vital")
+	FGameplayAttributeData Stamina;
+	UFUNCTION()
+	void OnRep_Stamina(const FGameplayAttributeData& OldValue);
+	ATTRIBUTE_ACCESSORS(USurvAttributeSet, Stamina)
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxStamina, Category="Attributes|Vital")
+	FGameplayAttributeData MaxStamina;
+	UFUNCTION()
+	void OnRep_MaxStamina(const FGameplayAttributeData& OldValue);
+	ATTRIBUTE_ACCESSORS(USurvAttributeSet, MaxStamina)
+
+	/** ---------------- Energy ---------------- */
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Energy, Category="Attributes|Vital")
+	FGameplayAttributeData Energy;
+	UFUNCTION()
+	void OnRep_Energy(const FGameplayAttributeData& OldValue);
+	ATTRIBUTE_ACCESSORS(USurvAttributeSet, Energy)
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxEnergy, Category="Attributes|Vital")
+	FGameplayAttributeData MaxEnergy;
+	UFUNCTION()
+	void OnRep_MaxEnergy(const FGameplayAttributeData& OldValue);
+	ATTRIBUTE_ACCESSORS(USurvAttributeSet, MaxEnergy)
+
+	/** ---------------- Thirst ---------------- */
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Thirst, Category="Attributes|Vital")
+	FGameplayAttributeData Thirst;
+	UFUNCTION()
+	void OnRep_Thirst(const FGameplayAttributeData& OldValue);
+	ATTRIBUTE_ACCESSORS(USurvAttributeSet, Thirst)
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxThirst, Category="Attributes|Vital")
+	FGameplayAttributeData MaxThirst;
+	UFUNCTION()
+	void OnRep_MaxThirst(const FGameplayAttributeData& OldValue);
+	ATTRIBUTE_ACCESSORS(USurvAttributeSet, MaxThirst)
+
+	/** ---------------- Hunger ---------------- */
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Hunger, Category="Attributes|Vital")
+	FGameplayAttributeData Hunger;
+	UFUNCTION()
+	void OnRep_Hunger(const FGameplayAttributeData& OldValue);
+	ATTRIBUTE_ACCESSORS(USurvAttributeSet, Hunger)
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHunger, Category="Attributes|Vital")
+	FGameplayAttributeData MaxHunger;
+	UFUNCTION()
+	void OnRep_MaxHunger(const FGameplayAttributeData& OldValue);
+	ATTRIBUTE_ACCESSORS(USurvAttributeSet, MaxHunger)
+	
+};
