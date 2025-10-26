6bba8cc04957c76b63cf04bba133f253ce3805d2
nektpol <nekocreativetechnologies@gmail.com>
2025-10-26 15:05:14 +0200

    Create enemy classes

---
+++ Source/FuturisticSurvival/Private/Character/SurvEnemyCharacter.cpp
@@ -0,0 +1,33 @@
+﻿// developed by Neko
+
+
+#include "Character/SurvEnemyCharacter.h"
+
+#include "AbilitySystemComponent.h"
+
+
+ASurvEnemyCharacter::ASurvEnemyCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+}
+
+UAbilitySystemComponent* ASurvEnemyCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void ASurvEnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!IsValid(AbilitySystemComponent))return;
+	GetAbilitySystemComponent()->InitAbilityActorInfo(this,this);
+
+	if (!HasAuthority())return;
+	GiveStartupAbilities();
+}
+
---
+++ Source/FuturisticSurvival/Public/Character/SurvEnemyCharacter.h
@@ -0,0 +1,26 @@
+﻿// developed by Neko
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SurvCharacter.h"
+#include "SurvEnemyCharacter.generated.h"
+
+UCLASS()
+class FUTURISTICSURVIVAL_API ASurvEnemyCharacter : public ASurvCharacter
+{
+	GENERATED_BODY()
+
+public:
+	ASurvEnemyCharacter();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+};
