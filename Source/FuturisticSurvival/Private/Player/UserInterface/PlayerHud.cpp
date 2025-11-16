d3a480cffdf947ce997ce187d990ef4d62ae4a1d
nektpol <nekocreativetechnologies@gmail.com>
2025-11-16 21:01:57 +0200

    Update HUD class

--- Source/FuturisticSurvival/Private/Player/UserInterface/PlayerHud.cpp
+++ Source/FuturisticSurvival/Private/Player/UserInterface/PlayerHud.cpp
@@ -2,3 +2,22 @@
 
 
 #include "Player/UserInterface/PlayerHud.h"
+
+#include "Components/Overlay.h"
+
+ESlateVisibility UPlayerHud::ToggleCharacterWindow(bool bUseOtherInventory)
+{
+	switch(CharacterMenuOverlay->GetVisibility())
+	{
+	case ESlateVisibility::Visible:
+		CharacterMenuOverlay->SetVisibility(ESlateVisibility::Collapsed);
+		break;
+	case ESlateVisibility::Collapsed:
+		CharacterMenuOverlay->SetVisibility(ESlateVisibility::Visible);
+		break;
+	default:
+		GEngine->AddOnScreenDebugMessage(-1,5.0f,FColor::Red,"UPlayerWidget::ToggleCharacterWindow - CharacterWindow visibility is not Visible or Collapsed");
+		break;
+	}
+	return CharacterMenuOverlay->GetVisibility();
+}
